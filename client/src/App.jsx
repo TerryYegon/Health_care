cff6b9c712ab2249d052ffa7ff7e90b7b31b4dbb
Terry Yegon <yegonterry618@gmail.com>
2025-09-26 02:38:47 +0300

    adding data to the new repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,17 @@
-// src/App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { useAuth } from "./context/AuthContext.jsx";
-import Appointments from "./pages/Appointments.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar.jsx";
+import Footer from "./components/Footer.jsx"; 
+import LandingPage from "./pages/LandingPage.jsx";
 
 function App() {
-  const { user, loading } = useAuth();
-
-  if (loading) {
-    return (
-      <div style={{ textAlign: "center", padding: "40px", color: "#6b7280" }}>
-        Loading...
-      </div>
-    );
-  }
-
   return (
     <Router>
+      <Navbar />
       <Routes>
-        {/* Default route */}
-        <Route
-          path="/"
-          element={
-            user ? <Appointments /> : <Navigate to="/login" replace />
-          }
-        />
-
-        {/* Appointments route */}
-        <Route
-          path="/appointments"
-          element={
-            user ? <Appointments /> : <Navigate to="/login" replace />
-          }
-        />
-
-        {/* Catch-all */}
-        <Route
-          path="*"
-          element={<Navigate to={user ? "/appointments" : "/"} replace />}
-        />
+        <Route path="/" element={<LandingPage />} />
+        {/* You can add more routes here */}
       </Routes>
+      <Footer />
     </Router>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AuthProvider } from './context/AuthContext'; 
 import App from './App.jsx';
-import './index.css'; 
+import './components/index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
